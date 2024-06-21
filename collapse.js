84c7f98f625d4507ec941c55294897ac46c8c1b5
Mayesha <104177936+GlitzyIsBored@users.noreply.github.com>
2024-06-21 15:19:14 +1000

    mehr zueg

---
+++ collapse.js
@@ -0,0 +1,12 @@
+console.log("Web first developed in 14/05/23. Last updated 21/06/24");
+
+const collapse = document.getElementById("option");
+
+collapse.addEventListener("click", action);
+function action() {
+    document.getElementById("nav").classList.toggle('active');
+    collapse.classList.toggle('click');
+
+}
+
+
